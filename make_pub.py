30a6b5548fe4a5b097883240ce27dedfe93dee53
nissmar <maruani.nissim@gmail.com>
2025-09-15 19:56:03 -0700

    publine

--- make_pub.py
+++ make_pub.py
@@ -9,7 +9,7 @@ conference = "SIGGRAPH Asia - Journal Track (TOG), 2025"
 abstract = "Our method introduces a novel differentiable mesh extraction framework that operates during the optimization of 3D Gaussian Splatting representations. At every training iteration, we differentiably extract a mesh—including both vertex locations and connectivity—only from Gaussian parameters. This enables gradient flow from the mesh to Gaussians, allowing us to promote bidirectional consistency between volumetric (Gaussians) and surface (extracted mesh) representations. This approach guides Gaussians toward configurations better suited for surface reconstruction, resulting in higher quality meshes with significantly fewer vertices. Our framework can be plugged into any Gaussian splatting representation, increasing performance while generating an order of magnitude fewer mesh vertices. MILo makes the reconstructions more practical for downstream applications like physics simulations and animation."
 
 text = f"""
-<ul class="pub">
+	<ul class="pub">
 		<div class="container">
 			<div class="zoomhover">
 				<a href="{webpage}">
@@ -27,6 +27,8 @@ text = f"""
 				<div class="pub-conf">{conference}</div>
 			</div>
 		</div>
-	</ul>"""
+	</ul>
+	<ul class="pub-line"></ul>
+	"""
 
 print(text)

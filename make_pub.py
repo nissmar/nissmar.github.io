39d15f15ffbc138c780847a43f8d05c43535aadb
nissmar <maruani.nissim@gmail.com>
2025-11-25 10:02:03 +0100

    id

--- make_pub.py
+++ make_pub.py
@@ -1,34 +1,47 @@
-webpage = "https://anttwo.github.io/milo/"
-img = "img/milo.gif"
-paper_name = "MILo: Mesh-In-the-Loop Gaussian Splatting for Detailed and Efficient Surface Reconstruction"
-authors = "Antoine Guédon, Diego Gomez, Nissim Maruani, Bingchen Gong, George Drettakis, Maks Ovsjanikov"
+webpage = "https://arxiv.org/abs/2511.17454"
+img = "img/illustrators_depth_square.pdf"
+paper_name = "Illustrator’s Depth: Monocular Layer Index Prediction for Image Decomposition"
+authors = "Nissim Maruani, Peiying Zhang, Siddhartha Chaudhuri, Matthew Fisher, Nanxuan Zhao, Vladimir G. Kim, Pierre Alliez, Mathieu Desbrun, Wang Yifan"
 authors = authors.replace("Nissim Maruani", "<b>Nissim Maruani</b>")
 
-conference = "SIGGRAPH Asia - Journal Track (TOG), 2025"
+conference = "Arxiv, 2025"
 
-abstract = "Our method introduces a novel differentiable mesh extraction framework that operates during the optimization of 3D Gaussian Splatting representations. At every training iteration, we differentiably extract a mesh—including both vertex locations and connectivity—only from Gaussian parameters. This enables gradient flow from the mesh to Gaussians, allowing us to promote bidirectional consistency between volumetric (Gaussians) and surface (extracted mesh) representations. This approach guides Gaussians toward configurations better suited for surface reconstruction, resulting in higher quality meshes with significantly fewer vertices. Our framework can be plugged into any Gaussian splatting representation, increasing performance while generating an order of magnitude fewer mesh vertices. MILo makes the reconstructions more practical for downstream applications like physics simulations and animation."
+abstract = "We introduce Illustrator’s Depth, a novel definition of depth that addresses a key challenge in digital content creation: decomposing flat images into editable, ordered layers. Inspired by an artist’s compositional process, illustrator’s depth infers a layer index for each pixel, forming an interpretable image decomposition through a discrete, globally consistent ordering of elements optimized for editability. We also propose and train a neural network using a curated dataset of layered vector graphics to predict layering directly from raster inputs. Our layer index inference unlocks a range of powerful downstream applications. In particular, it significantly outperforms state-of-the-art baselines for image vectorization while also enabling high-fidelity text-to-vector-graphics generation, automatic 3D relief generation from 2D images, and intuitive depth-aware editing. By reframing depth from a physical quantity to a creative abstraction, illustrator's depth prediction offers a new foundation for editable image decomposition."
 
+# DO NOT TOUCH
 text = f"""
-	<ul class="pub">
-		<div class="container">
-			<div class="zoomhover">
-				<a href="{webpage}">
-					<img class="pub-img" src="{img}" alt="Publication 1 Image">
-				</a>
-			</div>
-			<div>
-				<a class="pub-title" href="{webpage}"> {paper_name}
-					Diffusion</a>
-				<div class="pub-authors"> {authors} </div>
+        <ul class="pub">
+                <div class="container">
+                        <div class="zoomhover">
+                                <a href="{webpage}">
+                                        <img class="pub-img" src="{img}" alt="Publication 1 Image">
+                                </a>
+                        </div>
+                        <div>
+                                <a class="pub-title" href="{webpage}"> {paper_name}</a>
+                                <div class="pub-authors"> {authors} </div>
 
-				<div class="pub-abstract">
-					{abstract}
-				</div>
-				<div class="pub-conf">{conference}</div>
-			</div>
-		</div>
-	</ul>
-	<ul class="pub-line"></ul>
-	"""
+                                <div class="pub-abstract">
+                                        {abstract}
+                                </div>
+                                <div class="pub-conf">{conference}</div>
+                        </div>
+                </div>
+        </ul>
+        <ul class="pub-line"></ul>
+        """
 
-print(text)
+with open('index.html', 'r') as f:
+    html_text = f.read()
+    f.close()
+
+with open('index_backup.html', 'w') as f:
+    f.write(html_text)
+    f.close()
+html_text = html_text.replace(
+    "<h2>Publications</h2>", "<h2>Publications</h2> \n"+text)
+print(html_text)
+with open('index.html', 'w') as f:
+    f.write(html_text)
+    f.close()
+# print(text)
